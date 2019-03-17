d81411bc55caa695cf1f45b38b62cee96fbc0957
KazuhiroObama <obamakazuhiro@gmail.com>
2019-03-17 09:26:14 +0900

    ログインユーザーのみタスク管理可能
    管理者のみ管理機能実行可能処理

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 2019_03_16_011014) do
+ActiveRecord::Schema.define(version: 2019_03_17_000239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -8,7 +8,9 @@ ActiveRecord::Schema.define(version: 2019_03_16_011014) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["name"], name: "index_tasks_on_name", unique: true
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
